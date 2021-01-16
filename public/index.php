cebd43731212c0a10b335a85316df25ee8535e59
Kaveh Raji <kr@sleekcommerce.com>
2021-01-16 23:20:45 +0100

    (update) added the function that teaser - pics also can be added and shown in categories.

--- public/index.php
+++ public/index.php
@@ -56,7 +56,7 @@ $app->get('/reload-static-files', function () use ($app,$language,$menu,$usernam
   echo "WEBHOOK_EXECUTED";
 });
 
-$app->post('/add-coupon', function () use ($app,$language,$menu,$username,$cart,$discount) {
+$app->post('/add-coupon', function () use ($app,$language,$menu,$username,$cart) {
     $app->log->info("Slim-Skeleton '/' route");
     // Render index viewdd
     $coupon=$app->request->post("coupon");
